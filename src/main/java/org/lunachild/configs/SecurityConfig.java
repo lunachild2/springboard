31bd781201dcc3c647b96d3da334929bafeacdaa
lunachild <gkgktjddus@gmail.com>
2023-08-07 17:50:25 +0900

    스프링 시큐리티 기본 설정, 정적 경로 설정, 임시 메인 페이지 추가

---
+++ src/main/java/org/lunachild/configs/SecurityConfig.java
@@ -0,0 +1,21 @@
+package org.lunachild.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return w -> w.ignoring().requestMatchers("/css/**", "/js/**", "/images/**", "/errors/**");
+    }
+
+}
