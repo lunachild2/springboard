0017f99be9196c485b865fc91252956574ad27a7
lunachild <gkgktjddus@gmail.com>
2023-08-11 16:48:49 +0900

    휴대전화번호 검증 추가

--- src/main/java/org/lunachild/models/member/MemberSaveService.java
+++ src/main/java/org/lunachild/models/member/MemberSaveService.java
@@ -1,5 +1,11 @@
 package org.lunachild.models.member;
 
+import lombok.RequiredArgsConstructor;
+import org.lunachild.controllers.members.JoinForm;
+import org.lunachild.entities.Member;
+import org.lunachild.repositories.MemberRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,7 +13,20 @@ import org.springframework.stereotype.Service;
  * - 수정시 비밀번호는 값이 있을때만 수정
  */
 @Service
+@RequiredArgsConstructor
 public class MemberSaveService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void save(JoinForm joinForm) {
+
+        Member member = new ModelMapper().map(joinForm, Member.class);
+        member.setUserPw(passwordEncoder.encode(joinForm.getUserPw()));
+
+        memberRepository.saveAndFlush(member);
+    }
+
 
 
 }
--- src/main/java/org/lunachild/repositories/MemberRepository.java
+++ src/main/java/org/lunachild/repositories/MemberRepository.java
@@ -1,9 +1,22 @@
 package org.lunachild.repositories;
 
 import org.lunachild.entities.Member;
+import org.lunachild.entities.QMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface MemberRepository extends JpaRepository<Member, Long>,
         QuerydslPredicateExecutor<Member> {
+
+    // 아이디로 회원 조회
+    Member findByUserId(String userId);
+
+    // 아이디로 회원 존재 유무 체크
+    default boolean exists(String userId) {
+
+        QMember member = QMember.member;
+
+        return exists(member.userId.eq(userId));
+    }
+
 }
